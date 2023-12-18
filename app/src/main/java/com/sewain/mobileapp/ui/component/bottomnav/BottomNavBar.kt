6d6a7d4aecf318cf057e3c6648ebe4d4ac1d06f4
AhmadRayhan107 <ahmadrayhan45732@gmail.com>
2023-12-19 01:05:05 +0700

    clean some error code

--- app/src/main/java/com/sewain/mobileapp/ui/component/bottomnav/BottomNavBar.kt
+++ app/src/main/java/com/sewain/mobileapp/ui/component/bottomnav/BottomNavBar.kt
@@ -4,7 +4,6 @@ import android.content.res.Configuration
 import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -17,10 +16,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -28,7 +25,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.sewain.mobileapp.R
 import com.sewain.mobileapp.data.local.model.SessionModel
 import com.sewain.mobileapp.ui.navigation.Screen
 import com.sewain.mobileapp.ui.screen.detail_catalog.DetailCatalogScreen
@@ -41,9 +37,7 @@ import com.sewain.mobileapp.ui.screen.profile.ShopAccountScreen
 import com.sewain.mobileapp.ui.screen.profile.SocialMediaScreen
 import com.sewain.mobileapp.ui.theme.SewainAppTheme
 import com.sewain.mobileapp.ui.theme.SteelBlue
-import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeBottomNavBar(
     sessionModel: SessionModel,
@@ -126,11 +120,11 @@ fun HomeBottomNavBar(
             }
             composable(Screen.Profile.route) {
                 //call our composable screens here
-                ProfileScreen(navController, sessionModel)
+                ProfileScreen(navController = navController, sessionModel = sessionModel)
             }
             composable(Screen.DetailProfile.route) {
                 val id = it.arguments?.getString("id") ?: ""
-                DetailProfileScreen(id, navController, snackbarHostState)
+                DetailProfileScreen(id = id, navController = navController, snackbarHostState = snackbarHostState)
             }
             composable(Screen.ChangePassword.route) {
                 val id = it.arguments?.getString("id") ?: ""
--- app/src/main/java/com/sewain/mobileapp/ui/screen/detail_catalog/DetailCatalogScreen.kt
+++ app/src/main/java/com/sewain/mobileapp/ui/screen/detail_catalog/DetailCatalogScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
