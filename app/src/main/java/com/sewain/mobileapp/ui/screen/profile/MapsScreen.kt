be013d3d7868c038754eb19950598015554ab79c
AhmadRayhan107 <ahmadrayhan45732@gmail.com>
2023-12-20 15:44:35 +0700

    update view maps

--- app/src/main/java/com/sewain/mobileapp/ui/screen/profile/MapsScreen.kt
+++ app/src/main/java/com/sewain/mobileapp/ui/screen/profile/MapsScreen.kt
@@ -12,37 +12,49 @@ import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapsScreen(
     modifier: Modifier = Modifier,
 ) {
-    var mapView: MapView? = null
-    var googleMap: GoogleMap? by remember { mutableStateOf(null) }
-
-    AndroidView(
-        factory = { context ->
-            mapView = MapView(context).apply {
-                onCreate(null)
-                getMapAsync { map ->
-                    googleMap = map
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(0.0, 0.0), 10f))
-                    map.setOnMapLongClickListener { latLng ->
-                        map.addMarker(MarkerOptions().position(latLng))
-                    }
-                }
-            }
-            mapView as MapView
-        },
-        modifier = modifier.fillMaxSize(),
-        update = { mapView?.onResume() }
-    )
-    
-    DisposableEffect(Unit) {
-        onDispose {
-            mapView?.onDestroy()
-        }
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
     }
+    GoogleMap(
+        modifier = modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    )
+
+//    var mapView: MapView? = null
+//    var googleMap: GoogleMap? by remember { mutableStateOf(null) }
+
+//    AndroidView(
+//        factory = { context ->
+//            mapView = MapView(context).apply {
+//                onCreate(null)
+//                getMapAsync { map ->
+//                    googleMap = map
+//                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(0.0, 0.0), 10f))
+//                    map.setOnMapLongClickListener { latLng ->
+//                        map.addMarker(MarkerOptions().position(latLng))
+//                    }
+//                }
+//            }
+//            mapView as MapView
+//        },
+//        modifier = modifier.fillMaxSize(),
+//        update = { mapView?.onResume() }
+//    )
+//
+//    DisposableEffect(Unit) {
+//        onDispose {
+//            mapView?.onDestroy()
+//        }
+//    }
 }
